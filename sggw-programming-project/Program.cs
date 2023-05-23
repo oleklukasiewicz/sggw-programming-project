2e28805533ba7204d60656855d3a37ee426d6e9c
Aleksander Łukasiewicz <69370471+oleklukasiewicz@users.noreply.github.com>
2023-05-23 21:09:00 +0200

    test

--- sggw-programming-project/Program.cs
+++ sggw-programming-project/Program.cs
@@ -10,12 +10,9 @@ namespace sggw_programming_project
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("hello world");
-=======
+            Console.Write("now commit");
             Console.Write("test");
             Console.WriteLine("test 2 commit");
->>>>>>> 7fa833908a568d5196d3cbb5f22b7607f19a4016
         }
     }
 }
