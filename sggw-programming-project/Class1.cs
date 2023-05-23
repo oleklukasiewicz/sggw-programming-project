40cb46ecaf708619467caf2a8e0bd40be989b3a7
Aleksander Łukasiewicz <69370471+oleklukasiewicz@users.noreply.github.com>
2023-05-23 20:43:08 +0200

    Klasa init

--- sggw-programming-project/Class1.cs
+++ sggw-programming-project/Class1.cs
@@ -11,8 +11,8 @@ namespace sggw_programming_project
         public string Name { get; set; }
         public string Type { get; set; }
         private int Id { get; set; }
+
+        public DateTime Data { get; set; }
         public string Description { get; set; }
-        public Class1() { }
-        public Class1(string name) { }
     }
 }
