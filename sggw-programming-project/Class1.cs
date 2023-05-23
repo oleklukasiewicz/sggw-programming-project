6891163115670fa047ec073f89fae9fea07a9420
Aleksander Łukasiewicz <69370471+oleklukasiewicz@users.noreply.github.com>
2023-05-23 20:41:03 +0200

    Klasa commit

---
+++ sggw-programming-project/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sggw_programming_project
+{
+    internal class Class1
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        private int Id { get; set; }
+        public string Description { get; set; }
+        public Class1() { }
+        public Class1(string name) { }
+    }
+}
