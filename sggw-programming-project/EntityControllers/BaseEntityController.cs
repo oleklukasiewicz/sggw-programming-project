e24a6795a47a1c295de400088159aae7cbdf8d44
Aleksander Łukasiewicz <69370471+oleklukasiewicz@users.noreply.github.com>
2023-05-27 11:06:14 +0200

    Added entity controllers

---
+++ sggw-programming-project/EntityControllers/BaseEntityController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sggw_programming_project.EntityControllers
+{
+    internal class BaseEntityController
+    {
+        //klasa dla kontolera moba(entity) - klasa bedzie sterowała ruchem ora zachowaniem
+    }
+}
